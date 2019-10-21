db39854c93a6840bc27100064851f62d414e6c2f
Vanna Winland <vgwinland@gmail.com>
2019-10-21 19:10:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
@@ -8,19 +8,18 @@ class Student
     student_hash.each do |attribute, value|
       self.send("#{{attribute}=}", value)
     end
-    @@all << self 
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
